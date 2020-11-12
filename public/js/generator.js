9253ccbfca6032e98e65cbef196e94c471f43597
jleilani <jxleilani@gmail.com>
2020-11-11 19:53:59 -0500

    outfit gen page fixed linters

--- public/js/generator.js
+++ public/js/generator.js
@@ -1,11 +1,11 @@
 $("#refreshTops").on("click", () => {
-    $("#topsImage").html('<img src="https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg">');
+  $("#topsImage").html("<img src=\"https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg\">");
 });
 
 $("#refreshBottoms").on("click", () => {
-    $("#bottomsImage").html('<img src="https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg">');
+  $("#bottomsImage").html("<img src=\"https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg\">");
 });
 
 $("#refreshShoes").on("click", () => {
-    $("#shoesImage").html('<img src="https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg">');
+  $("#shoesImage").html("<img src=\"https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg\">");
 });
