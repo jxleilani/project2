70ae9c664d4ccc88c0d91606f62d4641496fe3bd
jleilani <jxleilani@gmail.com>
2020-11-11 20:02:31 -0500

    fix eslint

--- public/js/generator.js
+++ public/js/generator.js
@@ -1,11 +1,20 @@
 $("#refreshTops").on("click", () => {
-  $("#topsImage").html("<img src=\"https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg\">");
+  $("#topsImage").html(
+    // eslint-disable-next-line quotes
+    '<img src="https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg">'
+  );
 });
 
 $("#refreshBottoms").on("click", () => {
-  $("#bottomsImage").html("<img src=\"https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg\">");
+  $("#bottomsImage").html(
+    // eslint-disable-next-line quotes
+    '<img src="https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg">'
+  );
 });
 
 $("#refreshShoes").on("click", () => {
-  $("#shoesImage").html("<img src=\"https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg\">");
+  $("#shoesImage").html(
+    // eslint-disable-next-line quotes
+    '<img src="https://res.cloudinary.com/dzha9rezq/image/upload/c_scale,h_250/v1605027639/oiyd0svurzi8gxcrep3d.jpg">'
+  );
 });
